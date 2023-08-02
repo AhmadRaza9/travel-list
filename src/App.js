861cf3912997b9482451b5a742d4a750e06646f9
AhmadRaza9 <raza.ataki@gmail.com>
2023-08-02 17:08:51 +0500

    Rendering the Items List

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: false,
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 1,
+    packed: true,
+  },
+  {
+    id: 4,
+    description: "Power Bank",
+    quantity: 5,
+    packed: false,
+  },
+  {
+    id: 6,
+    description: "HandFree",
+    quantity: 10,
+    packed: false,
+  },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,7 +53,26 @@ function Form() {
   );
 }
 function PackgingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul className="list">
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {""}
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
