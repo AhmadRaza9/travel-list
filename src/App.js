77bb8c33e963aa2324a09dd276fc12f73acdffd1
AhmadRaza9 <raza.ataki@gmail.com>
2023-09-09 10:43:47 +0500

    Adding sort functionality

--- src/App.js
+++ src/App.js
@@ -83,10 +83,25 @@ function Form({ onAddItems }) {
 }
 
 function PackgingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             onDeleteItem={onDeleteItem}
@@ -95,6 +110,14 @@ function PackgingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <select className="actions" onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">Sort by input description</option>
+        <option value="packed">Sort by packed status</option>
+      </select>
+      <select className="actions">
+        <option value="packed">Clear all</option>
+      </select>
     </div>
   );
 }
